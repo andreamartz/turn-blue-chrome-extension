e2b54a70a501519abdadd1227993694cac7af44a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-10-13 11:10:38 -0500

    Add the word 'script' to comment

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ chrome.storage.sync.get('color', ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+// When the button is clicked, inject setPageBackgroundColor script into current page
 changeColor.addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -16,8 +16,7 @@ changeColor.addEventListener('click', async () => {
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
+// The body of this function will be executed as a content script inside the current page
 function setPageBackgroundColor() {
   chrome.storage.sync.get('color', ({ color }) => {
     document.body.style.backgroundColor = color;
